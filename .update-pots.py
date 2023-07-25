1874d1b44b07bb6b7e2b13cc7eb05883637d6b8d
Maciej Olko <maciej.olko@gmail.com>
2023-07-26 00:45:29 +0200

    Updating sources (#2)
    * Add manage script
    
    * Don't commit files where only POT-Creation-Date is changed, add git restore for clean ending state
    
    * Add an inline comment
    
    * Make venv before building
    
    * refactor
    
    * improve typing
    
    https://discuss.python.org/t/make-pathlib-extensible/3428/135
    
    * rename script and accept Python version as parameter
    
    * move docstring to script level
    
    * add github action
    
    * fix if in a step
    
    * fix script name in workflow
    
    * fix script invocation
    
    * fix git username
    
    * workflow: simplify pushing condition
    
    Co-authored-by: Rafael Fontenelle <rffontenelle@users.noreply.github.com>
    
    * workflow: set github-actions[bot] as commiter
    
    Co-authored-by: Rafael Fontenelle <rffontenelle@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Rafael Fontenelle <rffontenelle@users.noreply.github.com>

---
+++ .update-pots.py
@@ -0,0 +1,70 @@
+"""updates translation sources and commits them"""
+
+from argparse import ArgumentParser
+from contextlib import chdir
+from os import PathLike
+from pathlib import Path
+from re import match
+from shutil import move, rmtree
+from subprocess import call, run
+from tempfile import TemporaryDirectory
+
+
+def _update_pots(version: str) -> None:
+    _call('git diff --exit-code')
+    with TemporaryDirectory() as directory:
+        with chdir(directory):
+            _clone_cpython_repo(version)
+            _call('make -C cpython/Doc/ venv')
+            _build_gettext()
+        _replace_tree(Path(directory, 'cpython/Doc/locales/pot'), '.pot')
+    changed = _get_changed_pots()
+    added = _get_new_pots()
+    if all_ := changed + added:
+        _call(f'git add {" ".join(all_)}')
+        _call('git commit -m "Update sources"')
+    _call('git restore .')  # discard ignored files
+
+
+def _clone_cpython_repo(version: str):
+    _call(f'git clone -b {version} --single-branch https://github.com/python/cpython.git --depth 1')
+
+
+def _build_gettext():
+    _call(
+        "make -C cpython/Doc/ ALLSPHINXOPTS='-E -b gettext -D gettext_compact=0 -d build/.doctrees . locales/pot' build"
+    )
+
+
+def _replace_tree(source: PathLike, target: PathLike):
+    rmtree(target)
+    move(source, target)
+
+
+def _get_changed_pots() -> list[str]:
+    diff = _run("git diff -I'^\"POT-Creation-Date: ' --numstat")
+    return [match(r'\d+\t\d+\t(.*)', line).group(1) for line in diff.splitlines()]
+
+
+def _get_new_pots() -> list[str]:
+    ls_files = _run('git ls-files -o -d --exclude-standard')
+    return ls_files.splitlines()
+
+
+def _call(command: str):
+    if (return_code := call(command, shell=True)) != 0:
+        exit(return_code)
+
+
+def _run(command: str) -> str:
+    if (process := run(command, shell=True, capture_output=True)).returncode != 0:
+        exit(process.returncode)
+    return process.stdout.decode()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('version')
+    options = parser.parse_args()
+
+    _update_pots(options.version)
