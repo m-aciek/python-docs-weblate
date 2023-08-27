79d766f4787d3ad6ce44ed4f099a28e18ee14b6f
Maciej Olko <maciej.olko@affirm.com>
2023-08-28 00:11:23 +0200

    .update-pots.py: fix loading the commit

--- .update-pots.py
+++ .update-pots.py
@@ -31,6 +31,7 @@ def _update_pots(version: str) -> None:
                     _call('make -C cpython/Doc/ venv')
                     _build_gettext()
                     commit_message = _output('git -C cpython/ log --pretty=format:"%B" --max-count=1')
+                    cpython_commit = _output('git -C cpython/ rev-parse HEAD')
                 _replace_tree(Path(directory, 'cpython/Doc/locales/pot'), '.pot')
                 changed = _get_changed_pots()
                 added = _get_new_pots()
